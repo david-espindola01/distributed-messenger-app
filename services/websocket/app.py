bd3363d0d3456edaafde140455007f1e42ee1c67
soydavs <soydavs125@gmail.com>
2025-04-07 21:37:00 -0500

    error de sincronizacion con el websocket

--- services/websocket/app.py
+++ services/websocket/app.py
@@ -45,4 +45,4 @@ async def websocket_endpoint(websocket: WebSocket):
         print(f"Usuario desconectado: {user_id}")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=5003)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
