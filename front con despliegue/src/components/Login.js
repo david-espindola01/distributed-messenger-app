5394a8cc9fc520dc3ac4ec9fee9e7cc41b272ebb
david-espindola01 <david.espindola01@uptc.edu.co>
2025-06-09 20:58:22 -0500

    https

--- front con despliegue/src/components/Login.js
+++ front con despliegue/src/components/Login.js
@@ -16,7 +16,7 @@ const Login = () => {
 
     try {
       // Asegúrate de usar el protocolo correcto (http o https)
-      const response = await fetch('http://18.218.82.108:5000/login', {
+      const response = await fetch('https://18.218.82.108.nip.io:5000/login', {
         method: 'POST',
         headers: { 
           'Content-Type': 'application/json',
